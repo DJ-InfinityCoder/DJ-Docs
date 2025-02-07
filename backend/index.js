e27a3bffc3fdc4f80481b21f3f1d6f334ff9d2e3
DJ-InfinityCoder <161459648+DJ-InfinityCoder@users.noreply.github.com>
2025-02-08 00:44:01 +0530

    corsupdated

--- backend/index.js
+++ backend/index.js
@@ -13,8 +13,9 @@ const app = express();
 
 app.use(cors(
     { origin: process.env.FRONTEND_URL,
-        credentials: true,
-        methods: ["GET", "POST", "DELETE"]
+        methods: ["GET", "POST", "PUT", "DELETE"], 
+        allowedHeaders: ["Content-Type", "Authorization"], 
+        credentials: true 
      }
 ));
 
